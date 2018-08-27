e10a239a34eee78add3c0ba7dc943e1481f3473a
Anderson Melo <anderson.90@gmail.com>
2018-08-26 21:15:36 -0300

    AULA 2

--- nodejs-e-http/payfast/controllers/pagamentos.js
+++ nodejs-e-http/payfast/controllers/pagamentos.js
@@ -4,4 +4,41 @@ module.exports = function (app) {
         console.log('Recebida requisição de teste na por 3030.')
         res.send('OK.');
     });
+
+    app.post('/pagamentos/pagamento', function(req, res){
+
+        req.assert("forma_de_pagamento", "Forma de pagamento eh obrigatorio").notEmpty();
+        req.assert("valor", "Valor eh obrigatorio e deve ser um decimal").notEmpty().isFloat();
+        req.assert("moeda", "Moeda é obrigatória e deve ter 3 caracteres").notEmpty().len(3,3);
+
+        var erros = req.validationErrors();
+
+        if(erros){
+            console.log('Erros de validacao encontrados');
+            res.status(400).send(erros);
+            return;
+        }
+
+        var pagamento = req.body;
+        console.log('processando uma requisicao de um novo pagamento');
+
+        pagamento.status = 'CRIADO';
+        pagamento.data = new Date;
+
+        var connection = app.persistencia.connectionFactory();
+        var pagamentoDao = new app.persistencia.PagamentoDAO(connection);
+
+        pagamentoDao.salva(pagamento, function(erro, resultado){
+            if(erro){
+                console.log('erro ao inserir no banco: ' + erro);
+                res.status(500).send(erro);
+            }else{
+                console.log('pagameno criado' + resultado);
+                res.location('/pagamentos/pagamento/' + resultado.insertId);
+
+                res.status(201).json(pagamento);
+            }
+        });
+
+    });
 }
