0dfc3f75aa0ae92d9f4d2fc237a0d02ae3d2b150
Anderson Melo <anderson.90@gmail.com>
2018-09-06 15:53:27 -0300

    updates

--- nodejs-e-http/payfast/persistencia/PagamentoDAO.js
+++ nodejs-e-http/payfast/persistencia/PagamentoDAO.js
@@ -1,19 +1,15 @@
 function PagamentoDAO(connection) {
     this._connection = connection;
 }
-
 PagamentoDAO.prototype.salva = function(pagamento,callback){
     this._connection.query('INSERT INTO pagamentos SET ?', pagamento, callback);
 }
-
 PagamentoDAO.prototype.lista = function(callback){
     this._connection.query('select * from pagamentos',callback);
 }
-
 PagamentoDAO.prototype.buscaPorId = function(id,callback){
     this._connection.query('select * from pagamentos where id = ?',[id],callback);
 }
-
 module.exports = function(){
     return PagamentoDAO;
 }
